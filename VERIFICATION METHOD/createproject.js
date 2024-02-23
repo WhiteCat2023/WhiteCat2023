93e9f72be2f4a16aabfd1b94436a4cf503508a70
WhiteCat2023 <147495118+WhiteCat2023@users.noreply.github.com>
2024-02-23 12:39:06 +0900

    Add files via upload
    THIS IS MY VERIFICATION HTML

---
+++ VERIFICATION METHOD/createproject.js
@@ -0,0 +1,27 @@
+const inputs = document.querySelectorAll("input"),
+    button = document.querySelector("button");
+
+inputs.forEach((input, index1) => {
+    input.addEventListener("keyup", (e) => {
+        const currentInput = input,
+            nextInput = input.nextElementSibling, 
+            prevInput = input.previousElementSibling;
+        if(currentInput.value.length > 1){
+            currentInput.value = ""
+        }
+        if(nextInput && nextInput.hasAttribute("disabled") && currentInput.value !== ""){
+            nextInput.removeAttribute("disabled");
+            nextInput.focus();
+        }
+        if(e.key == "Backspace"){
+            inputs.forEach((input, index2) => {
+                if(index1 <= index2 && prevInput){
+                    input.setAttribute("disabled", true);
+                    currentInput.value = "";
+                    prevInput.focus();
+                }
+            });
+        }
+    });
+});
+window.addEventListener();
